c8cf852f702ef8bb40dfcaffb2a3228ceb4596a0
tofazzal19 <rana.ict337@gmail.com>
2025-07-28 12:45:57 +0600

    V-3

---
+++ friends.js
@@ -0,0 +1,23 @@
+let Treat = 80;
+
+
+if(Treat > 80 ){
+    console.log(' Let\'s go for a lunch');
+}
+     else  if(Treat >= 60 && Treat < 80){
+    
+     console.log('good luck next time.');
+     }
+    else if(Treat >40 && Treat < 60){
+         console.log('keep your friends message unseen.');
+    }
+    else if(Treat < 40){
+         console.log('block your friend.');
+    }
+ 
+    else {
+        console.log('go to home and sleep and act sad');
+    }
+ 
+
+ 
