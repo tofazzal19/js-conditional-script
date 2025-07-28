a26d078a9022d9d7f55abfdb8fa77d77bb193124
tofazzal19 <rana.ict337@gmail.com>
2025-07-28 11:38:40 +0600

    V-2

---
+++ grade.js
@@ -0,0 +1,17 @@
+let Grade = 2;
+
+if(Grade >= 90){
+    console.log('You got A')
+}
+else if (Grade >= 80){
+     console.log('You got B')
+}
+else if (Grade >= 70){
+     console.log('You got C')
+}
+else if (Grade >= 60){
+     console.log('You got D')
+}
+else{
+    console.log('You got F')
+}
