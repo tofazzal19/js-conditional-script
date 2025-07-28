ed28d568d1c6ea28dd30b6482264ef9686373749
tofazzal19 <rana.ict337@gmail.com>
2025-07-28 10:27:52 +0600

    first java commit

---
+++ bmi.js
@@ -0,0 +1,20 @@
+let BMI = 25;
+
+
+if(BMI  < 18.5){
+    console.log('you are underweight');
+}
+     else{
+     if(BMI >= 18.5 && BMI<=24.9){
+     console.log('you are normal.');
+ }
+    else{ if(BMI >=25 && BMI <= 29.9){
+         console.log('you are overweight.');
+    }
+    else{
+        console.log('you are obese.');
+    }
+ }
+
+ }
+ 
---
+++ food.js
@@ -0,0 +1,10 @@
+var Burger = 600;
+
+if(Burger > 500){
+      console.log("You get a free coke!");
+}
+
+else{
+ 
+     console.log("You need to pay 30 taka");
+}
