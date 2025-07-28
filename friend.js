8107ef943bd9ecafe058526fac91ab2d97d9aff7
tofazzal19 <131471407+tofazzal19@users.noreply.github.com>
2025-07-28 20:19:45 +0600

    nested java

---
+++ friend.js
@@ -0,0 +1,22 @@
+let tofa = 85;
+let friend = 75;
+
+if(tofa > 80){
+ if(friend > 80){
+    console.log('Let\'s go for lunch')
+}
+else if(friend >= 60 && friend < 80 ){
+    console.log('goodluck next time')
+}
+else if(friend >= 40 && friend < 60 ){
+    console.log('keep your friend\'s message unseen')
+}
+else if(friend < 40 ){
+    console.log('block your friend')
+}
+
+}
+
+else{
+    console.log('go to home and sleep and act sad')
+}
