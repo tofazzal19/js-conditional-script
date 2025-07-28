2bf32599d28aa57c020aa183bd6ebfedfc3d54e2
tofazzal19 <rana.ict337@gmail.com>
2025-07-28 14:16:24 +0600

    V-4

---
+++ ternary.js
@@ -0,0 +1,13 @@
+let num1 = 50;
+    num2 = 70;
+const result1 = num1 + num2;
+const result2 = num2 *2;
+
+if(num1 > num2){
+    console.log(result2);
+}
+ else{
+    console.log(result1);
+ }
+
+ (num1 > num2) ? result2 : result1;
